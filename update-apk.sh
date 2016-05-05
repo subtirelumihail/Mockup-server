2ed945eb041d3e72bc4cb29325507cd926e0a576
Subtirelu Mihail <dazzzz2007@gmail.com>
2016-05-05 22:05:06 +0300

    Update medfind release apk - $comment

---
+++ update-apk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git add .
+echo -n "Enter a comment for this release [ENTER]: "
+read comment
+git commit -m 'Update medfind release apk - $comment'
+git push
+ssh dazzzz2007@146.148.22.226 << 'bash update.sh'
