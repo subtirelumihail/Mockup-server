960982a52bd3d24369ca5c55665a1832620c8ad8
Subtirelu Mihail <mihail@devicehub.net>
2016-05-30 13:16:25 +0300

    Update medfind release apk - fix various

--- update-apk.sh
+++ update-apk.sh
@@ -4,4 +4,3 @@ echo -n "Enter a comment for this release [ENTER]: "
 read comment
 git commit -m "Update medfind release apk - $comment"
 git push
-ssh dazzzz2007@146.148.22.226 'cd server && bash update.sh'
